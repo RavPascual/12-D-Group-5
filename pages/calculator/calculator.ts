a789ab995138b430b8121c55ac03f691bf38a80a
RavPascual <rav.pascual150227@my.lsgh.edu.ph>
2020-09-17 21:29:48 +0800

    3 files modified

--- pages/calculator/calculator.ts
+++ pages/calculator/calculator.ts
@@ -1,3 +1,4 @@
+import { Output } from '@angular/compiler/src/core';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
@@ -9,18 +10,18 @@ import { NavController } from 'ionic-angular';
 export class CalculatorPage {
   newValue: number;
   oldValue: number;
-  financedAmount: number;
-  totalInterestPayable: number;
-  interestRate: 1.30;
+  financedAmount:number;
   paymentTerms: number;
+  totalInterestPayable: number;
+  interestRate: 0.013;
   monthlyPayment: number;
 
   constructor(public navCtrl: NavController) {
 
   }
 compute() {
-  this.financedAmount = (this.newValue - this.oldValue);
-    this.totalInterestPayable = (this.financedAmount * this.interestRate / 100) * this.paymentTerms;
-    this.monthlyPayment = (this.financedAmount - this.totalInterestPayable) / this.paymentTerms
+ this.financedAmount = (this.newValue - this.oldValue);
+ this.totalInterestPayable = (this.financedAmount * 0.0130 / 100) * this.paymentTerms;
+ this.monthlyPayment = (this.financedAmount - this.totalInterestPayable) / this.paymentTerms;
 }
 }
